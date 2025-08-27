a943bac1caeb9d95971210c1eed811cd2da40ad5
nishino-22 <m22122@g.metro-cit.ac.jp>
2025-08-27 15:40:01 +0900

    モールス信号からテキストへ変換するコードを追加

--- morse/lib/src/screens/camera.dart
+++ morse/lib/src/screens/camera.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:morse/src/screens/home.dart';
+import 'package:morse/src/screens/home.dart';
+import 'package:morse_code_generator/morse_code_generator.dart';
 
 class CameraPage extends StatefulWidget{
   const CameraPage({super.key});
@@ -8,6 +9,19 @@ class CameraPage extends StatefulWidget{
 }
 
 class _CameraPageState extends State<CameraPage> {
+  /*
+  final TextEditingController _morseController = TextEditingController();
+
+  String _textOutput = '';
+
+  void _convertMorseToText() {
+    setState(() {
+      String inputMorse = _morseController.text.trim();
+      _textOutput = morseToText(inputMorse);
+    });
+  }
+  */
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
