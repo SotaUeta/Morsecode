93b58ffff80cb6cab7b5871ac776e84d854e1af8
SotaUeta <112171358+SotaUeta@users.noreply.github.com>
2025-08-28 15:43:47 +0900

    Revert "Already scroll"

--- morse/lib/src/screens/home.dart
+++ morse/lib/src/screens/home.dart
@@ -72,26 +72,24 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Expanded(
-            child: Scrollbar(
-              child: ListView.builder(
-                itemCount: widget.rooms.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(color: Colors.deepPurpleAccent, width: 4),
-                      ),
+            child: ListView.builder(
+              itemCount: widget.rooms.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Colors.deepPurpleAccent, width: 4),
                     ),
-                    child: ListTile(
-                      leading: Icon(Icons.chat),
-                      title: Text(widget.rooms[index].roomName),
-                      onTap: () {
-                        widget.onRoomSelected(widget.rooms[index]);
-                      },
-                    )
-                  );
-                },
-              ),
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.chat),
+                    title: Text(widget.rooms[index].roomName),
+                    onTap: () {
+                      widget.onRoomSelected(widget.rooms[index]);
+                    },
+                  )
+                );
+              },
             ),
           ),
         ],
